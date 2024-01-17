7487cd5c3094ce5ccf0972f54021e27a44e2562f
Olga Seledets <olga.seledets@outlook.com>
2024-01-18 00:15:46 +0300

    Добавлено отображение todo с использованием Redux

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
+import { addTodo } from "../../slices/todoSlice";
 
 function App() {
+  const todoList = useSelector((state: RootState) => state);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const [value, setValue] = useState('');
+
   return (
     <div className="App">
       <h1>Дневной список дел</h1>
       <div className='wrapper'>
-        <input className='todo-input' placeholder='Добавить новый элемент'></input>
-        <button className='add-btn'>Добавить</button>
+        <input className='todo-input' onChange={(e) => setValue(e.target.value)} placeholder='Добавить новый элемент'></input>
+        <button className='add-btn' onClick={() => { dispatch(addTodo(value)) }}>Добавить</button>
       </div>
       <div className='todo-list'>
-        <div className='todo'>
-          <input type='checkbox' className='todo-checkbox'></input>
-          <p className='todo-item'>New list item</p>
-        </div>
-        <div className='todo'>
-          <input type='checkbox' className='todo-checkbox'></input>
-          <p className='todo-item'>New list item</p>
-        </div>
-        <div className='todo'>
-          <input type='checkbox' className='todo-checkbox'></input>
-          <p className='todo-item'>New list item</p>
-        </div>
+        {todoList.map((item) => (
+          <div className='todo' key={item.id}>
+            <input type='checkbox' className='todo-checkbox'></input>
+            <p className='todo-item'>{item.text}</p>
+          </div>
+        ))}
       </div>
       <hr></hr>
       <div className='wrapper-footer'>
