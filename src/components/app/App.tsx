d68d3e9d23645b92bdf17151e016f3659f0592c9
Olga Seledets <olga.seledets@outlook.com>
2024-01-18 19:29:19 +0300

    Добавлено изменение состояния todo

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
-import { addTodo } from "../../slices/todoSlice";
+import { addTodo, setStatus } from "../../slices/todoSlice";
 
 function App() {
   const todoList = useSelector((state: RootState) => state);
@@ -25,15 +25,15 @@ function App() {
       <div className='todo-list'>
         {todoList.map((item) => (
           <div className='todo' key={item.id}>
-            <input type='checkbox' className='todo-checkbox'></input>
-            <p className='todo-item'>{item.text}</p>
+            <input type='checkbox' className='todo-checkbox' style={{ backgroundColor: item.completed ? "#00D8A7" : "#FFFFFF" }} checked={item.completed} onChange={() => dispatch(setStatus({ completed: !item.completed, id: item.id }))}></input>
+            <p className='todo-item' style={{ textDecoration: item.completed ? "line-through" : "none" }}>{item.text}</p>
           </div>
         ))}
       </div>
       <div className='footer'>
         <hr className='line'></hr>
         <div className='progress-and-clear'>
-          <p>1/3 дело завершено</p>
+          <p>1/{todoList.length} дело завершено</p>
           <button className='clear-btn'>Очистить</button>
         </div>
       </div>
