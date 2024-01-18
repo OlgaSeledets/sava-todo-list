383c6a2c7eee7096756ab5e1966b40f061763a4c
Olga Seledets <olga.seledets@outlook.com>
2024-01-18 19:24:17 +0300

    Добавлен редьюсер setStatus

--- src/slices/todoSlice.ts
+++ src/slices/todoSlice.ts
@@ -1,9 +1,10 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { combineReducers, createSlice, nanoid } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
 interface Item {
   id: string
   text: string
+  completed: boolean
 }
 
 const todosSlice = createSlice({
@@ -16,11 +17,16 @@ const todosSlice = createSlice({
       },
       prepare: (text: string) => {
         const id = nanoid()
-        return { payload: { id, text } }
+        const completed = false
+        return { payload: { id, text, completed } }
       },
     },
+    setStatus: (state, action: PayloadAction<{completed: boolean; id: string}>) => {
+      const completedItemIndex = state.findIndex((item) => item.id === action.payload.id)
+      state[completedItemIndex].completed = action.payload.completed
+    }
   },
 })
 
-export const { addTodo } = todosSlice.actions;
+export const { addTodo, setStatus } = todosSlice.actions;
 export default todosSlice.reducer;
